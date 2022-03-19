2e2474cca2484843ab9bf5d77caabe1c1359a4d1
TAK848 <pairs_char0j@icloud.com>
2022-03-19 17:43:24 +0900

    add omikuji func

--- Sources/Omikuji/Omikuji.swift
+++ Sources/Omikuji/Omikuji.swift
@@ -1,6 +1,23 @@
-public struct Omikuji {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
+//public struct Omikuji {
+//    public private(set) var text = "Hello, World!"
+//
+//    public init() {
+//    }
+//}
+public enum Omikuji: String, CaseIterable {
+    case greatBlessing = "great blessing"
+    case goodBlessing = "good blessing"
+    case middleBlessing = "middle blessing"
+    case smallBlessing = "small blessing"
+    case uncertainLuck = "uncertain luck"
+    case badLuck = "bad luck"
+    case terribleLuck = "terrible luck"
+}
+extension Omikuji {
+    var description: String {
+        rawValue
+    }
+    static func draw() -> Self {
+        allCases.randomElement()!
     }
 }
